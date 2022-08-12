1dc40c3f6b2d25cbdfccfd7457c8ba65fbfe4a30
emreceylan71 <109828939+emreceylan71@users.noreply.github.com>
2022-08-12 21:49:03 +0300

    Update String_exercises.cpp

--- String_exercises.cpp
+++ String_exercises.cpp
@@ -1,21 +1,7 @@
 ﻿
 #include <iostream>
 using namespace std;
-/*
 
-int main()
-{
-	string word1;
-	cout << "Give us a word " << endl;
-	cin >> word1;
-
-	for (int i =word1.length() ; i >=0 ; i--) {
-
-		cout << word1[i];
-
-	}
-}
-*/
 
 int main()
 {
