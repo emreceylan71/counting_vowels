c5cbfd83509e455d1d47f92191827bb86c459887
emreceylan71 <109828939+emreceylan71@users.noreply.github.com>
2022-08-12 21:48:26 +0300

    Add files via upload

---
+++ String_exercises.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+using namespace std;
+/*
+
+int main()
+{
+	string word1;
+	cout << "Give us a word " << endl;
+	cin >> word1;
+
+	for (int i =word1.length() ; i >=0 ; i--) {
+
+		cout << word1[i];
+
+	}
+}
+*/
+
+int main()
+{
+	string word1;
+	int count = 0 ;
+	cout << "Give us a word and we will find how much vowels it has. " << endl;
+	cin >> word1;
+
+
+	for (int i = 0 ; i < word1.length(); i++) {
+
+		if (word1[i] == 'a' || word1[i] == 'e' || word1[i] == 'u' || word1[i] == 'i' || word1[i] == 'o') {
+			count++;
+		}
+		if (word1[i] == 'A' || word1[i] == 'E' || word1[i] == 'U' || word1[i] == 'I' || word1[i] == 'O') {
+			count++;
+		}
+
+	}
+	cout << "There are " << count << " vowels";
+
+	return 0;
+}
